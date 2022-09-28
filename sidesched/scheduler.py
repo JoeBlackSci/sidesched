d007c10a343d08c20f25fc0d24616bc11532bdd5
JoeBlackSci <73175400+JoeBlackSci@users.noreply.github.com>
2022-09-29 00:03:22 +0100

    :memo: Add method docstrings

--- sidesched/scheduler.py
+++ sidesched/scheduler.py
@@ -11,7 +11,7 @@ class Scheduler:
     def __init__(self, event: Event) -> None:
         self.event: Event = event
         self.cur_time: int = 0
-        self.side_decider: Callable = min
+        self.side_decider: Callable = min  # Suggested: [min, sum]
         self.feature_priority: List[str] = ["load", "spot", "side"]
         self._feature_func_dict: Dict[str, Callable] = {
             "load": self._get_load,
@@ -23,6 +23,18 @@ class Scheduler:
         #   mode: priotiy vs score (needs score modifiers?)
 
     def _fetch_timeslot(self, time: Optional[int] = None) -> Dict[str, List]:
+        """Fetch timeslot for given time from Event.
+
+        Parameters
+        ----------
+        time : Optional[int], optional
+            Position of timeslot, by default None
+
+        Returns
+        -------
+        Dict[str, List]
+            The timetable of sides at given time in format: {spot: [sides]}.
+        """
         if not time:
             time = self.cur_time
         return self.event.get_timeslot(time)
@@ -49,6 +61,20 @@ class Scheduler:
     def _get_freq_spot(
         self, timeslot: Dict[str, List], side: Side, *_, **__
     ) -> Dict[str, int]:
+        """Frequency of times side has been assigned each spot
+
+        Parameters
+        ----------
+        timeslot : Dict[str, List]
+            Timetable of sides at spots for a given time. {spot: [sides]}.
+        side : Side
+            Side to get frequency for.
+
+        Returns
+        -------
+        Dict[str, int]
+            Freqency side has previously been assigned spots. {spot: freq}
+        """
         freq_spot = self.event.freq_spot
         return {
             spot: freq_spot.loc[side, spot]  # type: ignore [custom index Side]
@@ -58,17 +84,48 @@ class Scheduler:
     def _get_freq_side(
         self, timeslot: Dict[str, List], side: Side, func: Optional[Callable] = None
     ) -> Dict[str, int]:
+        """Frequency side has previously been co-assigned spots with present sides.
+
+        Parameters
+        ----------
+        timeslot : Dict[str, List]
+            Timetable of sides at spots for a given time. {spot: [sides]}.
+        side : Side
+            Side to get frequency for.
+        func : Optional[Callable], optional
+            Function used to determine optimal assignment for the side.
+            Suggested: [min, sum]. minimal returned value is given priority.
+            None defaults to Sheduler.side_decider.
+
+        Returns
+        -------
+        Dict[str, int]
+            Frequency side has been co-assinged with sides at spots. {spot: freq}
+        """
 
         if not func:
             func = self.side_decider
         freq_side = self.event.freq_side
 
         return {
-            spot: sum(freq_side.loc[side, c_side] for c_side in sides)  # type: ignore [custom index Side]
+            spot: func(freq_side.loc[side, c_side] for c_side in sides)  # type: ignore [custom index Side]
             for spot, sides in timeslot.items()
         }
 
     def side_metrics(self, from_min: bool = False) -> Dict[str, Any]:
+        """Freqency sides have been assigned to spots and co-assigned with sides.
+
+        Parameters
+        ----------
+        from_min : bool, optional
+            Frequency is determined from zero rather than current global
+            minimum, by default False
+
+        Returns
+        -------
+        Dict[str, Any]
+            _description_
+        """
         freq_spot = self.event.freq_spot
         min_spots = freq_spot.to_numpy().min() if from_min else 0
         spots_at = [
@@ -90,7 +147,7 @@ class Scheduler:
     def _prioritise_sides(self, prio: Optional[str] = None) -> List[Side]:
         if not prio:
             prio = self.side_priority
-        
+
         metrics = self.side_metrics()
 
         if prio == "side":
@@ -112,16 +169,14 @@ class Scheduler:
         score_dict = {side: score for side, score in zip(metrics["sides"], scores)}
         return sorted(score_dict, key=lambda side: score_dict[side], reverse=True)
 
-
-######
     # def _schedule_next(self) -> None:
-    #     timeslot: Dict[str, List] = self._fetch_timeslot()
+    #     timeslot = self._fetch_timeslot()
     #     prio_sides = self._prioritise_sides()
-        
+
     #     side_decider =
     #     for feature in self.feature_priority:
     #     scores = self._feature_func_dict[feature](timeslot, side, self.side_decider)
-#######
+
 
 # def _shedule_next(self) -> None:
 
