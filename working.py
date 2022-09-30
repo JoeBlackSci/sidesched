f295f41a1e685791da85a5703b8b72fd710ebf80
JoeBlackSci <73175400+JoeBlackSci@users.noreply.github.com>
2022-09-30 11:45:19 +0100

    Add log config to example

--- working.py
+++ working.py
@@ -1,9 +1,12 @@
 from pprint import pprint
 from typing import List, Tuple, cast
+import logging
 
 import pandas as pd
 
 from sidesched import Event, Scheduler, Side
+
+logging.basicConfig(level=logging.DEBUG)
 
 names = ["earlsdon", "saddleworth", "cinewood", "SB&MRTD", "jockey", "shakespere"]
 spots = ["royal oak", "broomfeild", "chestnut"]
@@ -13,10 +16,35 @@ sides = [Side(name, size) for name, size in zip(names, sizes)]
 event = Event("bromyard", spots, sides, 3)
 
 schedule = Scheduler(event)
-schedule.schedule()
+schedule.side_priority = "spot"
+
+out = schedule._prioritise_sides()
+pprint(out)
+out = schedule.side_metrics()
+pprint(out)
+print()
+
+schedule._schedule_next()
+out = schedule._prioritise_sides()
+pprint(out)
+out = schedule.side_metrics()
+pprint(out)
+print()
+
+schedule._schedule_next()
+out = schedule._prioritise_sides()
+pprint(out)
+out = schedule.side_metrics()
+pprint(out)
+print()
+
+schedule._schedule_next()
+out = schedule._prioritise_sides()
+pprint(out)
+out = schedule.side_metrics()
+pprint(out)
+print()
+
 
 print(event.freq_side)
 print(event.freq_spot)
-
-
-
